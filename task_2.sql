502770e16f4c8c4d8927dfe766b0b3bcf00ce889
Gabriel Owusu <owusugabriel803@gmail.com>
2025-10-17 20:48:35 +0000

    table

--- task_2.sql
+++ task_2.sql
@@ -4,3 +4,44 @@ mycursor.execute("""
         author_name VARCHAR(215)
     ) 
 """)
+
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS Books (
+        book_id INT PRIMARY KEY,
+        title VARCHAR(130),
+        author_id INT, 
+        FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+        price DOUBLE,
+        publication_date DATE)
+    )
+
+""")
+
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS Customers (
+        customer_id INT PRIMARY KEY,
+        customer_name VARCHAR(215),
+        email VARCHAR(215) UNIQUE,  
+        address TEXT);
+    )
+
+""")
+
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS Orders (
+        order_id INT PRIMARY KEY,
+        customer_id INT,
+        FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+        order_date DATE);
+    )
+
+""")
+
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS Order_Details (
+        orderdetailid INT PRIMARY KEY,
+        order_id INT,
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+        book_id INT,
+        FOREIGN KEY (book_id) REFERENCES Books(book_id),
+        quantity DOUBLE)
