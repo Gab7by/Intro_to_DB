34f43a512bf932d6a53b6373b63212d90ba50602
Gabriel Owusu <owusugabriel803@gmail.com>
2025-10-18 22:43:19 +0000

    task 4

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,11 @@
-mycursor.execute("USE alx_book_store;" )
+import mysql.connector
+mdb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Mylove6711",
+        database="alx_book_store"
+    )
+
+mycursor = mdb.cursor()
 mycursor.execute("SELECT * FROM Books")
+print("successfully")
