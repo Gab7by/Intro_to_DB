68334af5413a13b7a2e9f1331359c404b89de39f
Gabriel Owusu <owusugabriel803@gmail.com>
2025-10-18 22:49:29 +0000

    table 4

--- task_4.sql
+++ task_4.sql
@@ -1,11 +1,1 @@
-import mysql.connector
-mdb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Mylove6711",
-        database="alx_book_store"
-    )
-
-mycursor = mdb.cursor()
-mycursor.execute("SELECT * FROM Books")
-print("successfully")
+INFORMATION_SCHEMA.COLUMNS", "COLUMN_NAME", "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books'
