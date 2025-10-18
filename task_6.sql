ca39a151ada06e2f3a1e389daf714993c5fb1ff3
Gabriel Owusu <owusugabriel803@gmail.com>
2025-10-18 23:25:46 +0000

    task 6

--- task_6.sql
+++ task_6.sql
@@ -9,15 +9,15 @@ mdb = mysql.connector.connect(
 mycursor = mdb.cursor()
 mycursor.execute("""
     INSERT INTO customers (customer_id, customer_name, email, address)
-    VALUES (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave.")
+    VALUES (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave.")
 """)
 
 mycursor.execute("""
     INSERT INTO customers (customer_id, customer_name, email, address)
-    VALUES (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave.")
+    VALUES (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave.")
 """)
 
 mycursor.execute("""
     INSERT INTO customers (customer_id, customer_name, email, address)
-    VALUES (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.")
+    VALUES (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.")
 """)
